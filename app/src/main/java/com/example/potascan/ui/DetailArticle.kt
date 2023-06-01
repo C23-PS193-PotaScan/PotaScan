40a1f417df52994b225d1bb428cf681f669648c8
Rafizi <naufalclas23@gmail.com>
2023-06-02 01:22:23 +0700

    Add layout article fitur

--- app/src/main/java/com/example/potascan/ui/DetailArticle.kt
+++ app/src/main/java/com/example/potascan/ui/DetailArticle.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.potascan.R
 
+
 class DetailArticle : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
