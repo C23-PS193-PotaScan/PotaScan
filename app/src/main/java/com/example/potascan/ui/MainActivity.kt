885ce170abee0d1f0268e9cf284ee955eaee9785
ShineDevi <shinedevi02@gmail.com>
2023-06-04 19:11:50 +0700

    nav item

--- app/src/main/java/com/example/potascan/ui/MainActivity.kt
+++ app/src/main/java/com/example/potascan/ui/MainActivity.kt
@@ -7,19 +7,26 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.potascan.R
+import com.example.potascan.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment)
 
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_camera, R.id.navigation_articles
-        ).build()
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_camera, R.id.navigation_articles
+            )
+        )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
