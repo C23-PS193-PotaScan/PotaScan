46889ee2372d419337b026c9c8faf38460b23ee0
Ahmad Robi <ahmad.robi8989@gmail.com>
2023-06-15 19:50:14 +0700

    create logout for the app

--- app/src/main/java/com/example/potascan/ui/MainActivity.kt
+++ app/src/main/java/com/example/potascan/ui/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.example.potascan.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.potascan.R
+import com.example.potascan.ViewModel.MainViewModel
 import com.example.potascan.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var isFirstInstall = false
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -48,16 +54,42 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.instruksi-> {
+            R.id.instruksi -> {
                 showInstructionDialog()
+            }
+
+            R.id.logOut -> {
+                showDialogChoose()
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
+    private fun showDialogChoose() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Androidly Alert")
+        builder.setMessage("We have a message")
+//builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+
+        builder.setNegativeButton("Yes") { _, _ ->
+            lifecycleScope.launch(Dispatchers.Main) {
+                mainViewModel.logout()
+            }
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+
+        builder.setPositiveButton("No") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
     private fun showInstructionDialog() {
         AlertDialog.Builder(this).apply {
-            val dialogView = LayoutInflater.from(this@MainActivity).inflate(R.layout.instruction_dialog, null)
+            val dialogView =
+                LayoutInflater.from(this@MainActivity).inflate(R.layout.instruction_dialog, null)
             val dialogBuilder = AlertDialog.Builder(this@MainActivity)
                 .setView(dialogView)
                 .setPositiveButton("OK") { dialog, _ ->
