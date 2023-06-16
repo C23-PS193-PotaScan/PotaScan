ccca652d1d66eab6acc5d2284d60816c263d1437
Ahmad Robi <ahmad.robi8989@gmail.com>
2023-06-16 12:03:27 +0700

    solved bug logOut

--- app/src/main/java/com/example/potascan/ui/MainActivity.kt
+++ app/src/main/java/com/example/potascan/ui/MainActivity.kt
@@ -29,9 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var isFirstInstall = false
-    private val mainViewModel by viewModels<MainViewModel> {
-        ViewModelFactoryArticle.getInstance(dataStore)
-    }
+    private lateinit var mainViewModel :MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
